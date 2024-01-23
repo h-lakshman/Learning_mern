1c0ad6d038a1e4a2fcd2e5de4df03395079aab01
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-01-23 18:18:50 +0530

    Made some changes in the CreateTodo component

--- todo-app/frontend/src/components/CreateTodo.jsx
+++ todo-app/frontend/src/components/CreateTodo.jsx
@@ -1,35 +1,31 @@
-import { useState } from "react";
-
+import { useState } from 'react'
 export function CreateTodo() {
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
 
   return <div>
-    <input type="text" placeholder="title" onChange={(e) => {
-      const value = e.target.value;
-      console.log(value)
+    <input type="text" placeholder="title" onChange={function(e) {
+      value = e.target.value
       setTitle(value)
     }}></input>
     <br></br> <br></br>
-    <input type="text" placeholder="description" onChange={(e) => {
-      const value = e.target.value;
-      console.log(value)
+    <input type="text" placeholder="description" onChange={function(e) {
+      value = e.target.value
       setDescription(value)
     }}></input>
     <br></br> <br></br>
     <button onClick={() => {
-      fetch("http:/localhost:3000/todo", {
+      fetch("http://localhost:3000/todo"), {
         method: "POST",
         body: JSON.stringify({
           title: title,
           description: description
         }),
         headers: {
-          "contentType": "application.json"
+          "Content-type": "application/json"
         }
-      })
+      }
         .then(async function(res) {
-          console.log(res.json)
           const json = await res.json();
           alert("Todo added")
         })
