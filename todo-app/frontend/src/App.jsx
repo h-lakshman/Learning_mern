f2646ae31556ddb4c5f95a56353f2edb05776688
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-01-25 23:31:12 +0530

    Made some changes in the fetch call

--- todo-app/frontend/src/App.jsx
+++ todo-app/frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
-    fetch("http://localhost:3000/todo/?id=1")
+    fetch("http://localhost:3000/todos")
       .then(async function(res) {
         const json = await res.json();
         setTodos(json.todos)
