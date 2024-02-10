e8b7ffbb44011bc842584345ae1d1fb9eb70f12e
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-02-10 17:45:37 +0530

    Added the Signup page and its components

---
+++ Paytm-clone/Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { SignUp } from './pages/SignUp'
+import { SignIn } from './pages/SignIn'
+import { Dashboard } from './pages/Dashboard'
+import SendMoney from './pages/SendMoney'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignUp />}></Route>
+          <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/send' element={<SendMoney />}></Route>
+        </Routes>
+      </BrowserRouter >
+    </>
+  )
+}
+
+export default App
---
+++ Paytm-clone/Frontend/src/components/InputBox.jsx
@@ -0,0 +1,10 @@
+export default function({ label, placeholder }) {
+  return (
+    <div>
+      <div className="text-sm font-medium text-left py-2">
+        {label}
+      </div>
+      <input type="text" placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+    </div>
+  )
+}
